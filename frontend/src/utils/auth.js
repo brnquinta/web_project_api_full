ac036b8f8599f82eec1a225bcf9489266969e527
BrunoQ <brn.quinta@gmail.com>
2026-02-24 21:05:13 -0300

    fix: remove extra fields from login request body

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -28,13 +28,13 @@ class AuthApi {
     }).then((res) => this._handleServerResponse(res));
   }
 
-  login({ email, password }) {
-    return fetch(`${this._baseUrl}/signin`, {
-      method: "POST",
-      headers: this._headers,
-      body: JSON.stringify({ email, password, name: "User", about: "About" }),
-    }).then((res) => this._handleServerResponse(res));
-  }
+login({ email, password }) {
+  return fetch(`${this._baseUrl}/signin`, {
+    method: "POST",
+    headers: this._headers,
+    body: JSON.stringify({ email, password }),
+  }).then((res) => this._handleServerResponse(res));
+}
 
   getUserAuth(token) {
     return fetch(`${this._baseUrl}/users/me`, {
